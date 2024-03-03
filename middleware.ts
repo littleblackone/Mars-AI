c8d2449efc421befa1ad5f10d25b67cfea9a7344
zsx <zsx352020833@gmail.com>
2024-03-03 14:40:47 +0800

    3

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,10 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ["/", "/api/webhooks(.*)"],
+  publicRoutes: ["/", "/api/webhook/clerk"],
   // Routes that can always be accessed, and have
   // no authentication information
-  ignoredRoutes: ["/"],
+  ignoredRoutes: ["/", '"/api/webhook/clerk"'],
 });
 
 export const config = {
