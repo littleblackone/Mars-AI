ccbc2ed70f5c142cfb25b8b1d94b8779cb7020b0
zsx <zsx352020833@gmail.com>
2024-03-06 22:14:02 +0800

    3

--- app/api/payCallback/route.ts
+++ app/api/payCallback/route.ts
@@ -4,8 +4,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 const handleCallback = async (req: NextRequest) => {
   try {
-    const body = await req.json();
-    console.log(body);
+    // const body = await req.json();
+    // console.log(body);
 
     // const timestamp = Math.floor(Date.now() / 1000); // 获取当前时间的秒级时间戳
     // const tenDigitTimestamp = timestamp.toString().substring(0, 10); // 提取前 10 位数
